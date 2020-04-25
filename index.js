051afe0901140f34e24dc4ec8439453b847ae93c
Pomppupallo <tonikvirtanen@gmail.com>
2020-04-25 16:51:44 +0300

    setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+let persons = [
+  {
+    name: "Matti Virtanen",
+    number: "041-123456",
+    id: 1,
+  },
+  {
+    name: "Toni Virtanen",
+    number: "041-321456",
+    id: 2,
+  },
+];
+
+app.get("/api/persons", (req, res) => res.json(persons));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on ${port} port!`));
