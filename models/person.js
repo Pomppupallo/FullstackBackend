519bacf39e7fb1411b54670decc25f0d7e89cac0
Pomppupallo <tonikvirtanen@gmail.com>
2020-05-07 14:10:43 +0300

    3.22: lint-konfiguraatio

--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -14,16 +14,16 @@ const personSchema = new mongoose.Schema({
     required: true,
     unigue: true
   }
-});
+})
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
